003090fa83dbb9f7284bdea1b6554f837e50b5ff
Lee Witek <neomech@gmail.com>
2015-08-02 23:22:07 +0100

    testing various libs

--- alfred.go
+++ alfred.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/davecgh/go-spew/spew"
+	"io/ioutil"
 	"net/http"
 )
 
 func githubnotify(w http.ResponseWriter, r *http.Request) {
-	spew.Print(r)
+	spew.Dump(r)
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		spew.Dump(err)
+	}
+	fmt.Println(string(data))
 }
 
 func main() {
