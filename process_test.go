975a88ddf4610979e525efa6e7f8feb231030e22
Lee Witek <neomech@gmail.com>
2015-08-15 22:50:09 +0100

    added tests

--- process_test.go
+++ process_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/simulatedsimian/assert"
 	"os"
 	"testing"
 	"time"
 )
 
 func TestExec(t *testing.T) {
-	execWithTimeout("go", "env", "/home/lmw/test", os.Stdout, 5*time.Second)
+	assert.Nil(t, execWithTimeout("dummyproc", "", "", os.Stdout, 100*time.Second))
+	assert.Nil(t, execWithTimeout("dummyproc", "5", "", os.Stdout, 100*time.Second))
+	assert.NotNil(t, execWithTimeout("dummyproc", "5", "", os.Stdout, 1*time.Second))
+	assert.NotNil(t, execWithTimeout("dummyproc", "5 1", "", os.Stdout, 100*time.Second))
 }
