911b82a565a987f24ef7772be41a833b018d1a31
Lee Witek <neomech@gmail.com>
2015-09-30 14:03:58 +0100

    added test and cover functions

--- builder.go
+++ builder.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -18,7 +19,14 @@ func builder(repos <-chan string) {
 	}
 }
 
-func goget(repo, gopath, logfile string) error {
+func generatePackageHash(pkg string) string {
+	return fmt.Sprintf("%x", sha1.Sum([]byte(pkg)))
+}
+
+func goget(pkg, logfile string) error {
+
+	gopath := generatePackageHash(pkg)
+
 	err := os.MkdirAll(gopath, os.ModePerm)
 	if err != nil {
 		return err
@@ -30,12 +38,54 @@ func goget(repo, gopath, logfile string) error {
 	}
 
 	log, err := os.Create(filepath.Join(gopath, logfile))
+	if err != nil {
+		return err
+	}
 	defer log.Close()
 
+	err = execWithTimeout("go", "get -v -u -t "+pkg+"/...", gopath, log, 300*time.Second)
+	return err
+}
+
+func gotest(pkg, logfile string) error {
+
+	gopath := generatePackageHash(pkg)
+	gopath, err := filepath.Abs(gopath)
 	if err != nil {
 		return err
 	}
 
-	err = execWithTimeout("go", "get -v -u -t "+repo+"/...", gopath, log, 30*time.Second)
+	log, err := os.Create(filepath.Join(gopath, logfile))
+	if err != nil {
+		return err
+	}
+	defer log.Close()
+
+	coverdata := filepath.Join(gopath, "coverdata.out")
+
+	args := fmt.Sprintf("test -v -covermode=count -coverprofile=%s %s", coverdata, pkg)
+	err = execWithTimeout("go", args, gopath, log, 300*time.Second)
+	return err
+}
+
+func gocover(pkg, logfile string) error {
+
+	gopath := generatePackageHash(pkg)
+	gopath, err := filepath.Abs(gopath)
+	if err != nil {
+		return err
+	}
+
+	log, err := os.Create(filepath.Join(gopath, logfile))
+	if err != nil {
+		return err
+	}
+	defer log.Close()
+
+	coverdata := filepath.Join(gopath, "coverdata.out")
+	html := filepath.Join(gopath, "coverdata.html")
+
+	args := fmt.Sprintf("tool cover -html=%s -o %s", coverdata, html)
+	err = execWithTimeout("go", args, gopath, log, 300*time.Second)
 	return err
 }
--- builder_test.go
+++ builder_test.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	//"fmt"
 	"github.com/simulatedsimian/assert"
 	"testing"
 )
 
+func TestPkgHash(t *testing.T) {
+	h1 := generatePackageHash("github.com/user/name")
+	h2 := generatePackageHash("github.com/user/name")
+	h3 := generatePackageHash("github.com/user/namex")
+
+	assert.Equal(t, h1, h2)
+	assert.NotEqual(t, h2, h3)
+}
+
 func TestGoGet(t *testing.T) {
-	assert.NoError(t, assert.Pack(goget("github.com/simulatedsimian/gocmdutil", "tempgopath", "buildlog")))
+	assert.NoError(t, assert.Pack(goget("github.com/simulatedsimian/assert", "buildlog")))
+	assert.NoError(t, assert.Pack(gotest("github.com/simulatedsimian/assert", "testlog")))
+	assert.NoError(t, assert.Pack(gocover("github.com/simulatedsimian/assert", "coverlog")))
 }
