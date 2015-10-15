dbeeb04ceac9901b996c16058abab50953a17b91
Lee Witek <neomech@gmail.com>
2015-10-16 00:01:51 +0100

    store all builds in data directory

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"os"
 )
@@ -9,10 +11,30 @@ type BuildQueuer interface {
 	EnQueue(pkg string)
 }
 
+func exitOnError(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
 func main() {
+	finfo, err := os.Stat("data")
+	if err == nil {
+		if !finfo.IsDir() {
+			exitOnError(errors.New("'data' present but not directory"))
+		}
+	} else {
+		err = os.Mkdir("data", os.ModePerm)
+		exitOnError(err)
+	}
+
+	err = os.Chdir("data")
+	exitOnError(err)
+
 	secretKey := []byte(os.Getenv("GITHUBSECRET"))
 	if len(secretKey) == 0 {
-		panic("GITHUBSECRET environment variable not set")
+		exitOnError(errors.New("GITHUBSECRET environment variable not set"))
 	}
 
 	webhook := GitHubWebHook{
